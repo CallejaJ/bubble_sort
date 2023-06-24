39c820315084ec52c492702b540551f7870832b3
Your Name <you@example.com>
2023-06-24 17:02:27 +0200

    bublesort2

--- bubblesort2.js
+++ bubblesort2.js
@@ -1,22 +1,17 @@
-// Dado un array de 1000 números aleatorios ordenarlos 
+// Dado un array de 10 números aleatorios ordenarlos 
 // de menor a mayor devolviendo el mismo array ordenado.
 
 const a = [];
-let numElementos = 10;
+let numElementos = 5;
 const MAX_INT = 20;
-// Esta constante establece el límite de numeros enteros.
-// Math.random() para obtener un número aleatorio entre 0 y 0,99999999
-// al multiplicarlo por la longitud de numElementos
-// obtenemos un número entre 0 y 9,999999999
-// Math.floor le quita la parte decimal
-// para Math.floor(4.99999999) devuelve 4.
-
-
 let j = 0;
 
+for(let i=0; i < numElementos; i++){
+    a[i] = Math.floor(Math.random() * MAX_INT);
+    console.log(a);
+}
 do{
     for(let i = 0; i < numElementos - j; i++){
-    a[i] = Math.floor(Math.random() * MAX_INT);
     if(a[i] > a[i+1]){
         let aux;
         aux = a[i];
